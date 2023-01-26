9f54ca6cc72ac907fa7cdd15a9f91d10e4dd04bb
Daniela Salas <danielapiconsalas@gmail.com>
2023-01-26 14:33:13 -0600

    break and continue from loops finisheed

--- js/break_continue.js
+++ js/break_continue.js
@@ -1,8 +1,16 @@
 "use strict";
 
 var usersOddNum = prompt("Enter an odd number.");
-for (i = 1; i <= 50; i++){
-    if(usersOddNum % 2 !== 0){
-        var askAgain = prompt("Try again. Enter an actual odd number.");
+
+    for (i = 1; i <= 50; i++) {
+        if (parseInt(usersOddNum) === i) {
+            console.log("Yikes! Skipping number: " + usersOddNum);
+        }
+        console.log("Here is the odd number: " + i);
     }
-}
+
+
+
+//if(usersOddNum % 2 !== 0){
+//         var askAgain = prompt("Try again. Enter an actual odd number.");
+//     }
