57d26326ac659a5cbc4b2cf8a0c27107b0dd6bb5
Daniela Salas <danielapiconsalas@gmail.com>
2023-02-03 16:18:50 -0600

    All problems from extra practice for assessment done

--- js/extra_practice.js
+++ js/extra_practice.js
@@ -35,7 +35,10 @@
 // }
 
 
-// Write a function named calculateTax that takes in two inputs representing the totalPaid and the taxPercent. If both inputs are numeric or numeric strings, calculateTax should return the total with tax added in a string looking like: “$XX.XX”. If either or both inputs are not numeric or numeric strings, calculateTax should return false.
+
+// Write a function named calculateTax that takes in two inputs representing the totalPaid and the taxPercent.
+// If both inputs are numeric or numeric strings, calculateTax should return the total with tax added in a string
+// looking like: “$XX.XX”. If either or both inputs are not numeric or numeric strings, calculateTax should return false.
 // calculateTax(25, 8)				 // “$27.00"
 // calculateTax(10, 12)             // “$11.20”
 // calculateTax(120, 15.5)			 //	“$138.60"
@@ -43,3 +46,13 @@
 // calculateTax([1, 2, 3], 10)      // false
 // calculateTax(“Codeup”, 100)      // false
 // calculateTax()                   // false
+//
+// function calculateTax(totalPaid, taxPercent){
+//     if((totalPaid === Number(totalPaid)) && (taxPercent === Number(taxPercent))) {
+//         var taxTotal = (taxPercent / 100) * totalPaid;
+//         var paidTax = totalPaid + taxTotal;
+//         return console.log("$" + paidTax.toFixed(2));
+//     }else{
+//         return false;
+//     }
+// }
