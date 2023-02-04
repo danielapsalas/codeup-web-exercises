62fde69e3870076618311abd53efd63378447df0
Daniela Salas <danielapiconsalas@gmail.com>
2023-02-04 13:12:48 -0600

    fixed someones code from the internet

--- js/extra_practice.js
+++ js/extra_practice.js
@@ -90,3 +90,21 @@
 // console.log(helloWorld('es'));
 // console.log(helloWorld('de'));
 // console.log(helloWorld('es'));
+
+//Correct this code
+// function colorMessage(favoriteColor, shirtColor) {
+//     if (favoriteColor === shirtColor) {
+//         return ‘The shirt is your favorite color!’;
+//     } else {
+//         return ‘That is a nice color.’; }}
+// console.log(colorMessage(‘black’, ‘white’));
+//
+//Corrected code
+function colorMessage(favoriteColor, shirtColor) {
+    if (favoriteColor === shirtColor) {
+        return "The shirt is your favorite color!";
+    } else {
+        return "That is a nice color.";
+    }
+}
+console.log(colorMessage("black", "white"));
