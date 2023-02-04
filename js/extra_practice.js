18c5c336c2f41193aeb8e7a831828b78c77b6f4f
Daniela Salas <danielapiconsalas@gmail.com>
2023-02-04 13:31:00 -0600

    largest of 5 function with user input done

--- js/extra_practice.js
+++ js/extra_practice.js
@@ -100,11 +100,40 @@
 // console.log(colorMessage(‘black’, ‘white’));
 //
 //Corrected code
-function colorMessage(favoriteColor, shirtColor) {
-    if (favoriteColor === shirtColor) {
-        return "The shirt is your favorite color!";
-    } else {
-        return "That is a nice color.";
+// function colorMessage(favoriteColor, shirtColor) {
+//     if (favoriteColor === shirtColor) {
+//         return "The shirt is your favorite color!";
+//     } else {
+//         return "That is a nice color.";
+//     }
+// }
+// console.log(colorMessage("black", "white"));
+
+// Write a JavaScript conditional statement to find the largest of five numbers.
+// Display an alert box to show the result.
+// Sample numbers : -5, -2, -6, 0, -1
+// Output : 0
+
+var num1 = prompt("Enter 1st number: ");
+var num2 = prompt("Enter 2nd number: ");
+var num3 = prompt("Enter 3rd number: ");
+var num4 = prompt("Enter 4th number: ");
+var num5 = prompt("Enter 5th number: ");
+
+function largestOf5(num1, num2, num3, num4, num5){
+    if(num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5){
+        alert("The largest num is: " + num1);
+    }else if(num2 > num1 && num2 > num3 && num2 > num4 && num2 > num5){
+        alert("The largest num is: " + num2);
+    }else if(num3 > num1 && num3 > num2 && num3 > num4 && num3 > num5){
+        alert("The largest num is: " + num3);
+    }else if(num4 > num1 && num4 > num2 && num4 > num3 && num4 > num5){
+        alert("The largest num is: " + num4);
+    }else if(num5 > num1 && num5 > num2 && num5 > num3 && num5 > num4){
+        alert("The largest num is: " + num5);
+    }else{
+        alert("ERROR");
     }
 }
-console.log(colorMessage("black", "white"));
+
+alert(largestOf5(num1, num2, num3, num4, num5));
