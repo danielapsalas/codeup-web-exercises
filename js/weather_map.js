940744723f82bbef985f95ba75253194c1f4a98b
Daniela Salas <danielapiconsalas@gmail.com>
2023-03-15 11:42:07 -0500

    current city, rows/colums, and css added

--- js/weather_map.js
+++ js/weather_map.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = mapBoxKey;
 const map = new mapboxgl.Map(
     {
         container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/outdoors-v12', // style URL
+        style: 'mapbox://styles/mapbox/dark-v11', // style URL
         center: [-98.491142,29.424349], // starting position [lng, lat]
         zoom: 10, // starting zoom (0 - 20)
     }
@@ -17,28 +17,31 @@ const marker = new mapboxgl.Marker({
 
 //marker portion
 marker.on('dragend', function(e){
+    let html = "";
     let longlat = e.target._lngLat;
     console.log(longlat);
     reverseGeocode({lat: longlat.lat, lng:longlat.lng}, mapBoxKey).then(function (results){
-        console.log(results);
         $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${results[1]}&lon=${results[0]}&appid=${openWeatherKey}&units=imperial`).done(function (data) {
             for(var i = 0; i <=39; i +=8) {
-                html += `<div>`
-                html += `<h5>${data.list[i].dt_txt}</h5>`
-                html += `<h6>${data.list[i].main.temp}</h6>`
+                html += `<div class="card col-2 mx-2" style="width: 14rem;>`
+                html += `<h5 class="card-header text-aligned-center">${data.list[i].dt_txt}</h5>`
+                html += `<h6 class="card-text"> ${data.list[i].main.temp}</h6>`
                 html += `<h6><img class="icons" src="http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png"</h6>`
-                html += `<h6>Description: ${data.list[i].weather[0].description}</h6>`
-                html += `<h6>Humidity: ${data.list[i].main.humidity}</h6>`
-                html += `<h6>Wind: ${data.list[i].wind.speed}</h6>`
-                html += `<h6>Pressure: ${data.list[i].main.pressure}</h6>`
+                html += `<h6 class="card-text">Description: ${data.list[i].weather[0].description}</h6>`
+                html += `<h6 class="card-text">Humidity: ${data.list[i].main.humidity}</h6>`
+                html += `<h6 class="card-text">Wind: ${data.list[i].wind.speed}</h6>`
+                html += `<h6 class="card-text">Pressure: ${data.list[i].main.pressure}</h6>`
+                html += `</div>`
                 html += `</div>`
             }
             $("#weatherBody").html(html);
+            html ="";
 
+            let city = "";
+            city += `<p class="d-flex justify-content-center">Current Location: ${data.city.name}</p>`
+            $("#current-city").html(city)
         })
-        html ="";
     })
-
 });
 
 
@@ -51,24 +54,29 @@ function geoCodeBuildWeather(searchString) {
             zoom: 10
         };
         map.flyTo(myOptionsObj);
+        marker.setLngLat(results);
 
         console.log(`https://api.openweathermap.org/data/2.5/forecast?lat=${results[1]}&lon=${results[0]}&appid=${openWeatherKey}&units=imperial`);
-
         $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${results[1]}&lon=${results[0]}&appid=${openWeatherKey}&units=imperial`).done(function (data) {
             for( var i = 0; i <=39; i +=8) {
-                html += `<div>`
-                html += `<h5>${data.list[i].dt_txt}</h5>`
-                html += `<h6>${data.list[i].main.temp}</h6>`
+                html += `<div class="card col-2 mx-2" style="width: 14rem;>`
+                html += `<h5 class="card-header text-aligned-center">${data.list[i].dt_txt}</h5>`
+                html += `<h6 class="card-text"> ${data.list[i].main.temp}</h6>`
                 html += `<h6><img class="icons" src="http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png"</h6>`
-                html += `<h6>Description: ${data.list[i].weather[0].description}</h6>`
-                html += `<h6>Humidity: ${data.list[i].main.humidity}</h6>`
-                html += `<h6>Wind: ${data.list[i].wind.speed}</h6>`
-                html += `<h6>Pressure: ${data.list[i].main.pressure}</h6>`
+                html += `<h6 class="card-text">Description: ${data.list[i].weather[0].description}</h6>`
+                html += `<h6 class="card-text">Humidity: ${data.list[i].main.humidity}</h6>`
+                html += `<h6 class="card-text">Wind: ${data.list[i].wind.speed}</h6>`
+                html += `<h6 class="card-text">Pressure: ${data.list[i].main.pressure}</h6>`
+                html += `</div>`
                 html += `</div>`
             }
             $("#weatherBody").html(html);
+            html ="";
+
+            let city = "";
+            city += `<p>Current Location: ${data.city.name}</p>`
+            $("#current-city").html(city);
         })
-        html ="";
     })
 }
 
@@ -77,3 +85,5 @@ $("#myBtn").click(function(e){
     e.preventDefault();
     geoCodeBuildWeather($("#searchInput").val());
 })
+
+geoCodeBuildWeather("San Antonio, TX");
