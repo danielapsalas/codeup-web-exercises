a21396dcfa737690bbff4e6efc1de5bc86b811cd
Daniela Salas <danielapiconsalas@gmail.com>
2023-03-16 13:59:29 -0500

    tenth to do finished - rewrite the following loop to use a for..of loop

--- js/es6.js
+++ js/es6.js
@@ -80,7 +80,7 @@ users.forEach(function(user) {
 let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
-developers.forEach(function (developer) {
+for(let developer of developers) {
 
   // TODO: rewrite the assignment below to use template strings
   list += '<li>' + developer + '</li>';
