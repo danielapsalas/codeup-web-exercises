e0739e9fa3274a31f6d839b86f634039ae5b31d2
Daniela Salas <danielapiconsalas@gmail.com>
2023-03-20 16:26:46 -0500

    part 1 of 2 done of assignment completed...Handle An API Promise

--- js/promises.js
+++ js/promises.js
@@ -1,6 +1,9 @@
 "use strict"
 
 function commitDay(username) {
-    fetch('http://api.github.com/users/' + username + '/events/public', {headers: {'Authorization': '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'}})
-        .then(response)
+    fetch('https://api.github.com/users/' + username + '/events/public', {headers: {'Authorization': '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'}})
+        .then(response => response.json())
+        .then(data => {console.log(data[0].created_at)})
 }
+
+commitDay("danielapsalas")
